01af776ae8c0db5e56d7a9326740e34941ab1a15
EmmanuelRamirezz <emmanuelrub_@hotmail.com>
2022-08-26 15:12:11 -0500

    Crecion de la seccion de figuras version desktop y mobile

--- figuras.js
+++ figuras.js
@@ -1,3 +1,13 @@
+const mobile__menu = document.querySelector('.mobile__menu');
+const mobileMenu__content = document.querySelector('.mobile-menu__content');
+
+mobile__menu.addEventListener("click", despliegue);
+
+function despliegue(){
+  console.log('menu de movil');
+  mobileMenu__content.classList.toggle('inactive');
+}
+
 console.group("Cuadrados");
 //codigo del cuadrado
 // const ladoCuadrado = 5;
