2d057fed830e2c8f6a677e5a0c5085b1ebc9803c
EmmanuelRamirezz <emmanuelrub_@hotmail.com>
2022-08-26 13:12:01 -0500

    se agregó el menú mobile y responsive a index y descuentos

--- descuentos.js
+++ descuentos.js
@@ -11,6 +11,15 @@
 //   porcentajePrecioConDescuento,
 //   precioConDescuento,
 // });
+const mobile__menu = document.querySelector('.mobile__menu');
+const mobileMenu__content = document.querySelector('.mobile-menu__content');
+
+mobile__menu.addEventListener("click", despliegue);
+
+function despliegue(){
+  console.log('menu de movil');
+  mobileMenu__content.classList.toggle('inactive');
+}
 
 function calcularPrecioConDescuento (precio, descuento){
   
