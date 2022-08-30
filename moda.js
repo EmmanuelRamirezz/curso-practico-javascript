50ebb95b1dfd7dd2a4cb04856d67f42a8289d55f
EmmanuelRamirezz <emmanuelrub_@hotmail.com>
2022-08-30 17:04:08 -0500

    Terminacion de todo de la seccion Promedio

--- moda.js
+++ moda.js
@@ -1,19 +1,30 @@
-const lista =[
-  1,2,3,1,2,3,4,2,2,2,1
-];
+const  modaInput =  document.querySelector('.moda__input');
+const  modaButton =  document.querySelector('.moda__button');
+const modaResult = document.querySelector('.moda__result');
 
-const  listaContador = {};
+modaButton.addEventListener('click', modaCalculo);
 
-//llenamos el objeto vacio
-lista.map(function(elemento){
+function modaCalculo(){
+  var modaValue=modaInput.value;
+  var modaArray =modaValue.split(',');
+  var modaArray2= modaArray.map(string =>{
+    return Number(string);
+  });
+
+  
+  
+  const  listaContador = {};
+
+  //llenamos el objeto vacio
+  modaArray2.map(function(elemento){
   if(listaContador[elemento]){
     listaContador[elemento]= listaContador[elemento]+1;
   }else{
     listaContador[elemento]=1;
   }
-});
+  });
 
-//convertimos el objeto en un array y lo ordenamos de menor a mayor
+  //convertimos el objeto en un array y lo ordenamos de menor a mayor
 const listaArray = Object.entries(listaContador).sort(
   function(ElementoA, ElementoB){
     return  ElementoA[1]-ElementoB[1];
@@ -29,12 +40,26 @@ function moda(lista1){
     }else{
       listaObj[elemento]=1;
     }
-  });
+    });
 
-  const listaArray1 = Object.entries(listaObj).sort(
+    const listaArray1 = Object.entries(listaObj).sort(
     function(ElementoA, ElementoB){
       return  ElementoA[1]-ElementoB[1];
     }
   );
   return listaArray1[listaArray1.length-1]
 }
+
+console.log(modaArray2);
+var arraychido =(listaArray[listaArray.length-1]);
+
+console.log(arraychido[0]);
+console.log(arraychido[1]);
+
+modaResult.innerText=('La moda de tus números es: '+arraychido[0]+' la cual se repite '+arraychido[1]+' veces')
+
+}
+
+
+
+
--- promedio.js
+++ promedio.js
@@ -1,35 +1,89 @@
-const lista1 =[
-  100,
-  200,
-  300,
-  500
-];
+const mobile__menu = document.querySelector('.mobile__menu');
+const mobileMenu__content = document.querySelector('.mobile-menu__content');
 
-var sumatoria=0;
+mobile__menu.addEventListener("click", despliegue);
 
-for(i=0; i<lista1.length; i++){
-  sumatoria= lista1[i]+sumatoria
+function despliegue(){
+  console.log('menu de movil');
+  mobileMenu__content.classList.toggle('inactive');
 }
-console.log(sumatoria/lista1.length);
+const promResult = document.querySelector('.prom__result');
+
+const promButton = document.querySelector('.prom__button');
+const promButton2 = document.querySelector('.prom__button2');
+const promButton3 = document.querySelector('.prom__button3');
+const promText = document.querySelector('.prom__text');
+const promInput = document.querySelector('.prom__input');
+const promContainer = document.querySelector('.prom__container');
+
+promButton.addEventListener("click", promCantidad);
+promButton2.addEventListener("click", promCalcular)
+promButton3.addEventListener("click", promRegresar);
+
+function promCantidad(){
+  promInput.classList.add('inactive1');
+  promButton.classList.add('inactive');
+  promButton2.classList.remove('inactive');
+  promButton3.classList.remove('inactive');
+  promText.innerText=('Ingrese sus numeros por favor');
+
+  for(i=0; i<promInput.value; i++){
+    promContainer.classList.remove('inactive1');
+
+    const input1 = document.createElement('input');
+    input1.classList.add('prom__input');
+    input1.setAttribute('type', 'number');
+    input1.id=(i);
+    
+    promContainer.appendChild(input1);
+  }
+}
+function promCalcular(){
+  var arreglo = [];
+  for(j=0; j<promInput.value; j++){
+    const valor = document.getElementById(j);
+    valorChido=parseFloat(valor.value);
+    arreglo.push(valorChido);
+  }
+  console.log(arreglo);
+  var sumatoria=0;
+
+  for(i=0; i<arreglo.length; i++){
+  sumatoria= arreglo[i]+sumatoria
+  }
+  var promRes=sumatoria/arreglo.length;
+  console.log(promRes);
+  promResult.innerText=('El promedio de tus numeros es: '+promRes);
+}
+function promRegresar(){
+  promButton.classList.remove('inactive');
+  promButton2.classList.add('inactive');
+  promButton3.classList.add('inactive');
+  promInput.classList.remove('inactive1');
+  promText.innerText=('¿Cuantos numeros va a ingresar?');
+  promContainer.classList.add('inactive1');
+  
+  for(i=0; i<promInput.value; i++){
+    promContainer.removeChild(promContainer.firstElementChild);
+  }
+  arreglo=[]
+  promResult.innerText=("");
+}
 
 
+
+
+//Promedio usando el metodo reduce
 function CalcularMedia(lista){
-
-  // var sumatoria1=0;
-  // for(i=0; i<lista.length; i++){
-  //   sumatoria1= lista[i]+sumatoria1
-  // }
-
   const sumarLista=lista.reduce(
     function(valorAcumulado=0, nuevoElemento){
       return valorAcumulado+nuevoElemento;
     }
   );
-    
   return sumarLista/lista.length;
 }
 
-//Media usando el metodo reduce
+
 
 
   
